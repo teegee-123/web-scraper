592fdbc2909b411fe897351ae7e55408f560d9c1
teegee-123 <u10132806@gmail.com>
2024-09-19 16:41:26 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM ghcr.io/puppeteer/puppeteer:19.7.2
 
-ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true\
-    CHROMIUM_EXECUTABLE_PATH=/usr/bin/google-chrome-stable\
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
     PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome-stable
 
 WORKDIR /usr/src/app
@@ -9,4 +8,5 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm ci
 COPY . .
-CMD ["node", "index.js"] 
+CMD [ "node", "index.js" ]
+
--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const express = require('express');
-const scraper = require('./run-scraper')
+const express = require("express");
+const { scrapeLogic } = require("./scrapeLogic");
+const app = express();
 
-const app  = express()
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
+app.get("/scrape", (req, res) => {
+  scrapeLogic(res);
+});
 
+app.get("/", (req, res) => {
+  res.send("Render Puppeteer server is up and running!");
+});
 
 app.listen(PORT, () => {
-    console.log("listening on port "+ PORT)
-})
-
-app.get("/", (req, res)=> {
-    res.send("UP")
-})
-
-app.get("/scrape", (req, res)=> {
-    scraper.runScraper(res)
-})
+  console.log(`Listening on port ${PORT}`);
+});
